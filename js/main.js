a40a67b93f6c678e08345d2520bf715a41b91a2c
AsadbekBahromov21 <bahromov1621a@gmail.com>
2024-06-12 06:41:19 +0300

    js006

--- js/main.js
+++ js/main.js
@@ -136,3 +136,104 @@
         }
     // console.log(son(4, 3, 7));
 }
+{
+    // 15 masala
+    function son(a, b, c, d) {
+                if (a === b && b === c) return 4;
+                if (a === b && b === d) return 3;
+                if (a === c && c === d) return 2;
+                if (b === c && c === d) return 1;
+                return 0;
+            }
+            
+            // console.log(son(1, 5, 2, 4));
+    
+}
+
+
+{
+    // 16 masala
+    function sonlar( a, b, c) {
+        if ( a < b && b < c) {
+            return `sonlar ${a} ${b} ${c} 
+            Result: a son ${a *= 2} ; b son ${b *= 2} ; c son ${c *= 2};`
+        }
+        else{
+            return `Result = ${-a} , b = ${-b}, c = ${-c} `
+        }
+    }
+        //  console.log(sonlar(2,4,6));
+        //  console.log(sonlar(4,8,2));
+
+}
+
+{
+    // 17 masala
+    function son( a, b, c) {
+        if ((a < b && b < c) || (a > b && b > c) ) {
+            return  `sonlar ${a} ${b} ${c} 
+            Result: a son ${a *= 2} ; b son ${b *= 2} ; c son ${c *= 2};`
+        }else{
+        
+            return `Result = ${-a} , b = ${-b}, c = ${-c} `
+        }
+    }
+
+    // console.log(son(4,7,2));
+    // console.log(son(2,6,8));
+}
+
+{
+    // 18 masala
+    function son(a,b,c) {
+        if ( a === b) {
+            return `${a} = ${b} va c: ${c} soning tartib raqami` 
+        } else if(a === c){
+       
+            return`${a} = ${c} va  b: ${b} soning tartib raqami 2` 
+        } else if(b === c){
+       
+            return `${b} = ${c} va a: ${a} soning tartib raqami 1`
+        }
+    }
+
+    //  console.log(son( 3, 3, 2) );
+    //  console.log(son (2,5,2) );
+    //  console.log( son (3, 5, 5));
+
+}
+
+{
+    // 19 masala
+    function son(a , b, c, n) {
+        if (a === b && a === c) {
+            return  `${a} = ${b} = ${c} n:${n} soning tartib raqami 4`
+        } else if (a === b && a === n){
+            return `${a} = ${b} = ${n} c:${c} soning tartib raqami 3`
+        } else if (a === c && a === n){
+            return `${a} = ${c} = ${n} b:${b} soning tartib raqami 2`
+        } else if (b === c && b === n){
+            return `${b} = ${c} = ${n} a:${a} soning tartib raqami 1`
+        }    
+    }
+
+    // console.log( son (2,2,2,5));
+    // console.log(son (2,2,6,2));
+    // console.log(son (5,2,5,5));
+    // console.log(son (3,5,5,5));
+}
+
+{
+    // 20 masala
+    function son(a, b, c) {
+                let masofaAB = Math.abs(a-b);
+                let masofaAC = Math.abs(a-c);
+                if (masofaAB < masofaAC) {
+                    return `eng yaqin nuqta b, masofa ${masofaAB}`
+                } else {
+                    return  `eng yaqin masofa c, masofa ${masofaAC}`
+                }
+            }
+            // console.log( son (6, 2, 8) );
+            // console.log( son (5, 1, 10) );
+}
